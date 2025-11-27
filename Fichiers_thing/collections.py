4f2365c6e1f39548368471e0eb28351428e24a7c
sam <2233299@cegepoutaouais.qc.ca>
2025-11-27 15:06:24 -0500

    added fichier thing

---
+++ Fichiers_thing/collections.py
@@ -0,0 +1,26 @@
+# 1. Liste simple
+fruits = ["Pomme", "Banane", "Orange"]
+
+# 2. Dictionnaire simple
+capitales = {"France": "Paris", "Canada": "Ottawa", "Japon": "Tokyo"}
+
+# 3. Liste 2D (ex: grille)
+notes = [
+    [12, 15, 17],
+    [14, 10, 18],
+    [19, 11, 16]
+]
+
+# 4. Liste de dictionnaires (ex: élèves)
+eleves = [
+    {"nom": "Alice", "age": 14},
+    {"nom": "Bob", "age": 15},
+    {"nom": "Chloé", "age": 14}
+]
+
+# 5. Dictionnaire de listes (ex: matières -> notes)
+bulletins = {
+    "Math": [12, 14, 18],
+    "Français": [15, 11, 16],
+    "Histoire": [17, 10, 19]
+}
